91d3000eccd731b04bfd2f52e7de1f2a47b5d28c
sajjad1-moeis <154022929+sajjad1-moeis@users.noreply.github.com>
2023-12-22 12:49:29 +0330

    sl

--- js/Hedear-Site.js
+++ js/Hedear-Site.js
@@ -415,7 +415,7 @@ function CreateMahsolBasket(arr, dot) {
       <div class="w-full flex justify-between border-b-[1px] border-zinc-500 py-2">
       <div class="flex w-full">
       <div>
-      <img src="${dot}${item.img}" class="w-20" alt="" />
+      <img src="${item.img}" class="w-20" alt="" />
       </div>
       <div class="my-auto ms-4">
       <p>${item.title}</p>
